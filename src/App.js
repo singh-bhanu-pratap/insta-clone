27b06d50a0bb520abc7acef76ae59362487f720d
singh-bhanu-pratap <141299455+singh-bhanu-pratap@users.noreply.github.com>
2023-09-06 16:14:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Homepage from "./Homepage";
 import Authentication from "./authentication/Authentication";
+import { auth } from "./firebase";
+import { loginUser, setLoading } from "./features/userSlice";
+
 
 function App() {
-  const user = useSelector(state => state.data.user)
-  console.log(user);
-  
+  const dispatch = useDispatch();
 
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          loginUser({
+            uid: authUser.uid,
+            username: authUser.displayName,
+            email: authUser.email,
+          })
+        );
+        dispatch(setLoading(false));
+      } else {
+        dispatch(setLoading(false));
+        console.log("User is not logged in");
+      }
+    });
+  }, []); 
+
+  const user = useSelector((state) => state.data.user.user);
+  const isLoading = useSelector((state) => state.data.user.isLoading);
+  
   return (
     <div className="app">
-      {/*<Homepage />*/}
-
-      <Authentication />
+      {isLoading ? (  <div className="loader-container">
+      <div className="loader"></div>
+      </div>) :(<>{user ? <Homepage /> : <Authentication />}</>) }
+    
     </div>
   );
-}
-
+  }
+ 
 export default App;

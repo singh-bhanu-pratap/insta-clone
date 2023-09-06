1c015b874af925aafa92e74ef62e995fffdf05b8
singh-bhanu-pratap <141299455+singh-bhanu-pratap@users.noreply.github.com>
2023-09-06 15:56:59 +0530

    Update reducers.js

--- src/app/reducers.js
+++ src/app/reducers.js
@@ -1,8 +1,6 @@
 import { combineReducers } from "redux";
 import { userSlice } from "../features/userSlice";
 
-
-
 export const rootReducer = combineReducers({
-    user: userSlice.reducer,
+  user: userSlice.reducer,
 });
